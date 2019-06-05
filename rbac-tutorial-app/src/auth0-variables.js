4e828f25f628a43db92c0d5a7f34e607e4d73e37
kanikas01 <larvakurt@yahoo.com>
2019-06-05 10:17:54 -0700

    Add completed tutorial

---
+++ rbac-tutorial-app/src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "xyzz.auth0.com",
+  roleUrl: "https://rbac-tutorial-app/role",
+  clientId: "S9fmZsw80z7dDhciWwk16mQTotj21V37",
+  callbackUrl: "http://localhost:3000/callback"
+};
